cae96176eb3ec12ac6f6e2192a0688d2c541b7b4
Andrew Dirksen <andrew@dirksen.com>
2019-07-19 12:36:45 -0700

    assemble node template from most recent substrate master

---
+++ README.md
@@ -0,0 +1,30 @@
+# Substrate Warmup
+
+SRML-based Substrate node derived from [substrate node template](https://github.com/paritytech/substrate/tree/afa583011eed3e8d49ee823257a3b448a24e213b/node-template) and [substrate-package](https://github.com/shawntabrizi/substrate-package/tree/bc114c30521f5d7b84cfffb6be59f886309679d5/substrate-node-template).
+
+# Requires
+
+```bash
+# Rust
+curl https://sh.rustup.rs -sSf | sh
+
+# wasm32-unknown-unknown target
+rustup target add wasm32-unknown-unknown --toolchain nightly
+
+# wasm-gc
+cargo +nightly install --git https://github.com/alexcrichton/wasm-gc --force
+```
+
+# Run
+
+You can start a development chain with:
+
+```bash
+cargo run -- --dev
+
+# or with detailed logs
+RUST_LOG=debug RUST_BACKTRACE=1 cargo run -- --dev
+
+# or get available command line options
+cargo run -- --help
+```
---
+++ src/service.rs
@@ -0,0 +1,134 @@
+//! Service and ServiceFactory implementation. Specialized wrapper over Substrate service.
+
+use basic_authorship::ProposerFactory;
+use consensus::{import_queue, start_aura, AuraImportQueue, SlotDuration};
+use futures::prelude::*;
+use inherents::InherentDataProviders;
+use log::info;
+use network::{config::DummyFinalityProofRequestBuilder, construct_simple_protocol};
+use primitives::{ed25519::Pair, Pair as PairT};
+use std::sync::Arc;
+use substrate_client::{self as client, LongestChain};
+use substrate_executor::native_executor_instance;
+use substrate_service::construct_service_factory;
+use substrate_service::{
+    error::Error as ServiceError, FactoryFullConfiguration, FullBackend, FullClient,
+    FullComponents, FullExecutor, LightBackend, LightClient, LightComponents, LightExecutor,
+};
+use substrate_warmup_runtime::{self, opaque::Block, GenesisConfig, RuntimeApi, WASM_BINARY};
+use transaction_pool::{self, txpool::Pool as TransactionPool};
+
+pub use substrate_executor::NativeExecutor;
+
+// Our native executor instance.
+native_executor_instance!(
+	pub Executor,
+	substrate_warmup_runtime::api::dispatch,
+    substrate_warmup_runtime::native_version,
+	WASM_BINARY
+);
+
+#[derive(Default)]
+pub struct NodeConfig {
+    inherent_data_providers: InherentDataProviders,
+}
+
+construct_simple_protocol! {
+    /// Demo protocol attachment for substrate.
+    pub struct NodeProtocol where Block = Block { }
+}
+
+construct_service_factory! {
+    struct Factory {
+        Block = Block,
+        RuntimeApi = RuntimeApi,
+        NetworkProtocol = NodeProtocol { |config| Ok(NodeProtocol::new()) },
+        RuntimeDispatch = Executor,
+        FullTransactionPoolApi = transaction_pool::ChainApi<
+            client::Client<FullBackend<Self>, FullExecutor<Self>, Block, RuntimeApi>,
+            Block
+        > {
+            |config, client| Ok(TransactionPool::new(config, transaction_pool::ChainApi::new(client)))
+        },
+        LightTransactionPoolApi = transaction_pool::ChainApi<
+            client::Client<LightBackend<Self>, LightExecutor<Self>, Block, RuntimeApi>,
+            Block
+        > {
+            |config, client| Ok(TransactionPool::new(config, transaction_pool::ChainApi::new(client)))
+        },
+        Genesis = GenesisConfig,
+        Configuration = NodeConfig,
+        FullService = FullComponents<Self>
+            { |config: FactoryFullConfiguration<Self>|
+                FullComponents::<Factory>::new(config)
+            },
+        AuthoritySetup = {
+            |service: Self::FullService| {
+                if let Some(key) = service.authority_key::<Pair>() {
+                    info!("Using authority key {}", key.public());
+                    let proposer = Arc::new(ProposerFactory {
+                        client: service.client(),
+                        transaction_pool: service.transaction_pool(),
+                    });
+                    let client = service.client();
+                    let select_chain = service.select_chain()
+                        .ok_or_else(|| ServiceError::SelectChainRequired)?;
+                    let aura = start_aura(
+                        SlotDuration::get_or_compute(&*client)?,
+                        Arc::new(key),
+                        client.clone(),
+                        select_chain,
+                        client,
+                        proposer,
+                        service.network(),
+                        service.config.custom.inherent_data_providers.clone(),
+                        service.config.force_authoring,
+                    )?;
+                    service.spawn_task(Box::new(aura.select(service.on_exit()).then(|_| Ok(()))));
+                }
+
+                Ok(service)
+            }
+        },
+        LightService = LightComponents<Self>
+            { |config| <LightComponents<Factory>>::new(config) },
+        FullImportQueue = AuraImportQueue<
+            Self::Block,
+        >
+            { |config: &mut FactoryFullConfiguration<Self> , client: Arc<FullClient<Self>>, _select_chain: Self::SelectChain| {
+                    import_queue::<_, _, Pair>(
+                        SlotDuration::get_or_compute(&*client)?,
+                        Box::new(client.clone()),
+                        None,
+                        None,
+                        client,
+                        config.custom.inherent_data_providers.clone(),
+                    ).map_err(Into::into)
+                }
+            },
+        LightImportQueue = AuraImportQueue<
+            Self::Block,
+        >
+            { |config: &mut FactoryFullConfiguration<Self>, client: Arc<LightClient<Self>>| {
+                    let fprb = Box::new(DummyFinalityProofRequestBuilder::default()) as Box<_>;
+                    import_queue::<_, _, Pair>(
+                        SlotDuration::get_or_compute(&*client)?,
+                        Box::new(client.clone()),
+                        None,
+                        None,
+                        client,
+                        config.custom.inherent_data_providers.clone(),
+                    ).map(|q| (q, fprb)).map_err(Into::into)
+                }
+            },
+        SelectChain = LongestChain<FullBackend<Self>, Self::Block>
+            { |config: &FactoryFullConfiguration<Self>, client: Arc<FullClient<Self>>| {
+                #[allow(deprecated)]
+                Ok(LongestChain::new(client.backend().clone()))
+            }
+        },
+        FinalityProofProvider = { |_client: Arc<FullClient<Self>>| {
+            Ok(None)
+        }},
+    }
+}
